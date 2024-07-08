730fd997cee08c735299ae3caee178181f287ab1
pdferrao <pdferrao@myseneca.ca>
2024-07-07 20:59:57 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
-*  WEB700 – Assignment 3
+*  WEB700 – Assignment 4
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including 3rd party web sites) or distributed to other students.
@@ -84,6 +84,7 @@ app.get("/", function(req, res) {
 app.get("/about", function(req, res) {
     res.sendFile(path.join(__dirname, "/views/about.html")); // Send the about.html file as the response
 });
+
 
 // Route for the HTML demo page
 app.get("/htmlDemo", function(req, res) {
